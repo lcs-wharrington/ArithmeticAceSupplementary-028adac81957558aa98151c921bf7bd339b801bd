1ef7b647bba66220bff08dd7ae7c39229153b40e
William Harrington <wharrington@lcs.on.ca>
2022-02-22 17:35:44 -0500

    Initial Commit

---
+++ ArithmeticAce/ArithmeticAceApp.swift
@@ -0,0 +1,42 @@
+//
+//  ArithmeticAceApp.swift
+//  ArithmeticAce
+//
+//  Created by Russell Gordon on 2022-02-07.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticAceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+            TabView {
+                AdditionView()
+                    .tabItem {
+                        Image(systemName: "plus.circle")
+                        Text("Add")
+                    }
+
+                SubtractionView()
+                    .tabItem {
+                        Image(systemName: "minus.circle")
+                        Text("Subtract")
+                    }
+                
+                MultiplicationView()
+                    .tabItem {
+                        Image(systemName: "multiply.circle")
+                        Text("Multiply")
+                    }
+                
+                DivisionView()
+                    .tabItem {
+                        Image(systemName: "divide.circle")
+                        Text("Divide")
+                    }
+            }
+        }
+    }
+}
---
+++ ArithmeticAce/Views/MultiplicationView.swift
@@ -0,0 +1,145 @@
+//
+//  MultiplicationView.swift
+//  ArithmeticAce
+//
+//  Created by Russell Gordon on 2022-02-07.
+//
+
+import SwiftUI
+
+struct MultiplicationView: View {
+    
+    // MARK: Stored properties
+    @State var multiplicand = Int.random(in: 1...12)
+    @State var multiplier = Int.random(in: 1...12)
+    
+    // This string contains whatever the user types in
+    @State var inputGiven = ""
+    
+    // Has an answer been checked?
+    @State var answerChecked = false
+    
+    // Was the answer given actually correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+    // What is the correct product?
+    var correctProduct: Int {
+        return multiplicand * multiplier
+    }
+    
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            HStack {
+                Text("✕")
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("\(multiplicand)")
+                    Text("\(multiplier)")
+                }
+            }
+            
+            Divider()
+            
+            HStack {
+                ZStack {
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                    //        CONDITION      true  false
+                        .opacity(answerCorrect == true ? 1.0 : 0.0)
+                    
+                    Image(systemName: "x.square")
+                        .foregroundColor(.red)
+                    //        CONDITION1         AND     CONDITION2         true  false
+                    //       answerChecked = true     answerCorrect = false
+                        .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
+                    
+                    
+                }
+                Spacer()
+                TextField("",
+                          text: $inputGiven)
+                    .multilineTextAlignment(.trailing)
+            }
+            
+            ZStack {
+                
+                Button(action: {
+                    
+                    // Answer has been checked!
+                    answerChecked = true
+                    
+                    // Convert the input given to an integer, if possible
+                    guard let productGiven = Int(inputGiven) else {
+                        // Sadness, not a number
+                        answerCorrect = false
+                        return
+                    }
+                    
+                    // Check the answer!
+                    if productGiven == correctProduct {
+                        // Celebrate! 👍🏼
+                        answerCorrect = true
+                    } else {
+                        // Sadness, they gave a number, but it's correct 😭
+                        answerCorrect = false
+                    }
+                }, label: {
+                    Text("Check Answer")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                // Only show this button when an answer has not been checked
+                    .opacity(answerChecked == false ? 1.0 : 0.0)
+                
+                Button(action: {
+                    // Generate a new question
+                    multiplicand = Int.random(in: 1...12)
+                    multiplier = Int.random(in: 1...12)
+                    
+                    // Reset properties that track what's happening with the current question
+                    answerChecked = false
+                    answerCorrect = false
+                    
+                    // Reset the input field
+                    inputGiven = ""
+                }, label: {
+                    Text("New question")
+                        .font(.largeTitle)
+                })
+                    .padding()
+                    .buttonStyle(.bordered)
+                // Only show this button when an answer has been checked
+                    .opacity(answerChecked == true ? 1.0 : 0.0)
+                
+            }
+            
+            // Reaction animation
+            ZStack {
+                LottieView(animationNamed: "51926-happy")
+                    .opacity(answerCorrect == true ? 1.0 : 0.0)
+                    .padding()
+
+                LottieView(animationNamed: "91726-sad-guy-is-walking")
+                    .opacity(answerChecked == true && answerCorrect == false ? 1.0 : 0.0)
+                    .padding()
+            }
+            
+            Spacer()
+        }
+        .padding(.horizontal)
+        .font(.system(size: 72))
+        
+        
+    }
+}
+
+struct MultiplicationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MultiplicationView()
+    }
+}
